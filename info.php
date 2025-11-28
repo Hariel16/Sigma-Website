f66a0495ba49cae13e0f824332ff49525e4ac5c9
Hariel16 <ajavonhariel@gmail.com>
2025-11-28 11:15:17 +0000

    delete everything

--- info.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Restrict access to admin users only
-if (!isset($_SESSION['admin_logged_in']) || !$_SESSION['admin_logged_in']) {
-    http_response_code(403);
-    exit('Access denied.');
-}
-phpinfo();
-?>
